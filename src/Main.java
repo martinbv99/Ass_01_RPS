4af75a0044ed4e49d39a584c53f32b4e2ccd04a6
martinbv99 <martinbv@mail.uc.edu>
2024-09-26 15:44:33 -0400

    final rps

--- src/Main.java
+++ src/Main.java
@@ -14,36 +14,66 @@ public class Main
         System.out.print("Choose your move player 2 [RPS]: ");
         play2 = in.nextLine();
 
-                    if (play1.equalsIgnoreCase("R")) {
-                        if (play2.equalsIgnoreCase("R")) {
-                            System.out.println("Rock vs Rock, It's a tie! ");
-                        } else if (play2.equalsIgnoreCase("P")) {
-                            System.out.println("Paper covers Rock, Player 2 wins!");
-                        } else {
-                            System.out.println("Rock breaks Scissors, Player 1 wins!");
-                        }
-                    }
+        if (play1.equalsIgnoreCase("R"))
+        {
+            if (play2.equalsIgnoreCase("R"))
+               {
+                System.out.println("Rock vs Rock, It's a tie! ");
+               }
+            else if (play2.equalsIgnoreCase("P"))
+               {
+                System.out.println("Paper covers Rock, Player 2 wins!");
+               }
+            else if (play2.equalsIgnoreCase("S"))
+               {
+                System.out.println("Rock breaks Scissors, Player 1 wins!");
+               }
+            else
+               {
+                   System.out.println("Invalid move! Try again! ");
+               }
+        }
 
-                    if (play1.equalsIgnoreCase("P")) {
-                        if (play2.equalsIgnoreCase("R")) {
-                            System.out.println("Paper covers Rock, Player 1 wins! ");
-                        } else if (play2.equalsIgnoreCase("P")) {
-                            System.out.println("Paper vs Paper, It's a tie! ");
-                        } else {
-                            System.out.println("Scissors cuts Paper, Player 2 wins!");
-                        }
-                    }
+        if (play1.equalsIgnoreCase("P"))
+        {
+            if (play2.equalsIgnoreCase("R"))
+               {
+                   System.out.println("Paper covers Rock, Player 1 wins! ");
+               }
+            else if (play2.equalsIgnoreCase("P"))
+               {
+                   System.out.println("Paper vs Paper, It's a tie! ");
+               }
+            else if (play2.equalsIgnoreCase("S"))
+               {
+                   System.out.println("Scissors cuts Paper, Player 2 wins!");
+               }
+            else
+               {
+                   System.out.println("Invalid move! Try again!");
+               }
+        }
 
-                    if (play1.equalsIgnoreCase("S")) {
-                        if (play2.equalsIgnoreCase("R")) {
-                            System.out.println("Rock breaks Scissors, Player 2 wins! ");
-                        } else if (play2.equalsIgnoreCase("P")) {
-                            System.out.println("Scissors cuts Paper, Player 1 wins! ");
-                        } else {
-                            System.out.println("Scissors vs Scissors, It's a tie! ");
-                        }
+        if (play1.equalsIgnoreCase("S"))
+        {
+            if (play2.equalsIgnoreCase("R"))
+               {
+                   System.out.println("Rock breaks Scissors, Player 2 wins! ");
+               }
+            else if (play2.equalsIgnoreCase("P"))
+               {
+                   System.out.println("Scissors cuts Paper, Player 1 wins! ");
+               }
+            else if (play2.equalsIgnoreCase("S"))
+               {
+                   System.out.println("Scissors vs Scissors, It's a tie! ");
+               }
+            else
+               {
+                   System.out.println("Invalid move! Try again!");
+               }
 
-                    }
+        }
 
 
     }
